b3098e27f12a5ac83cadb8f9752f8a8df6cca397
Frederic G. Fleron <fleron.frederic@gmail.com>
2024-12-03 00:11:28 +0100

    Added the functionalities to add new blog posts

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
 
+def load_posts():
+    with open("blog_posts.json", "r") as file:
+        return json.load(file)
+
+
+def save_posts(posts):
+    with open("blog_posts.json", "w") as file:
+        json.dump(posts, file, indent=4)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template("index.html", posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == "POST":
+        blog_posts = load_posts()
+
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        new_id = max(post["id"] for post in blog_posts) + 1 if blog_posts else 1
+
+        new_post = {
+            "id": new_id,
+            "author": author,
+            "title": title,
+            "content": content
+        }
+
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+
+        return redirect(url_for("index"))
+
+    return render_template("add.html")
 
 
 if __name__ == '__main__':
